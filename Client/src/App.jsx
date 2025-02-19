e4cd980263e13553f82279cad379a25207e802ed
dev-code-create <ayushkankale04@gmail.com>
2025-02-19 18:36:55 +0530

    Last

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import Header from "./components/Header";
 import About from "./components/About";
 import Projects from "./components/Projects";
-import Testimonials from "./components/testimonials";
+
 import Contact from "./components/Contact";
 import { ToastContainer } from "react-toastify";
 import Footer from "./components/Footer";
-
+import Testimonials from "./components/Testimonials";
 const App = () => {
   return (
     <div className="w-full overflow-hidden">
